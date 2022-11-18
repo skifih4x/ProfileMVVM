2ef5c6f6c8959897a53f2a9b990afbf50858ae5c
Artem <artem-orlov92@bk.ru>
2022-11-18 18:03:21 +0300

    Initial Commit

---
+++ ProfileMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileMVVM
+//
+//  Created by Артем Орлов on 18.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
